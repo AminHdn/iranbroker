714ce904288441d9e97e30d0d281a0f266121457
AminHdn <haidarian.amin@gmail.com>
2024-08-22 14:11:14 +0330

    update : responsive2

--- components/PriceContainer.vue
+++ components/PriceContainer.vue
@@ -1,35 +1,50 @@
 <template>
   <div>
     <div
-      class="flex flex-row mx-16 my-2 outline outline-1 outline-slate-200 px-2 py-4 rounded"
+      class="lg:mx-16 my-2 outline outline-1 outline-slate-200 md:px-2 md:py-4 rounded hover:bg-[#4D7A91] hover:text-white"
     >
-      <div class="basis-1/6 flex justify-start">
-        <img class="ibw-exchange-logo" :src="model?.img" width="25px" />
-        <p class="mx-1">{{ model?.title }}</p>
-      </div>
-      <div class="basis-1/6 flex justify-center">
-        {{
-          toPersianNumber(
-            currency == "btc" ? model?.btc.sell : model?.usdt.sell
-          )
-        }}
+      <div class="flex flex-row">
+        <div class="basis-4/12 lg:basis-1/6 block md:flex justify-start">
+          <img class="ibw-exchange-logo" :src="model?.img" width="25px" />
+          <p class="mx-1 text-[#2898F2] m-auto text-sm md:text-base">
+            {{ model?.title }}
+          </p>
+        </div>
+        <div
+          class="basis-4/12 lg:basis-1/6 flex justify-center text-sm md:text-base"
+        >
+          {{
+            toPersianNumber(
+              currency == "btc" ? model?.btc.sell : model?.usdt.sell
+            )
+          }}
 
-        تومان
+          تومان
+        </div>
+        <div class="basis-4/12 lg:basis-1/6 flex justify-center text-sm">
+          {{
+            toPersianNumber(
+              currency == "btc" ? model?.btc.buy : model?.usdt.buy
+            )
+          }}
+          تومان
+        </div>
+        <div class="basis-1/6 md:flex justify-center hidden">
+          %{{ toPersianNumber(model?.fee) }}
+        </div>
+        <div class="basis-1/6 md:flex justify-center hidden">
+          {{ model?.type }}
+        </div>
+        <div class="basis-1/6 md:flex justify-center hidden">
+          {{ toPersianNumber(model?.coinsCount) }}
+        </div>
+
+        <div class="md:basis-1/6 md:flex justify-center hidden">
+          <button class="bg-[#4CAF50] text-white p-1 w-1/2">خرید</button>
+          <button class="bg-[#DC3545] text-white p-1 mr-1 w-1/2">فروش</button>
+        </div>
       </div>
-      <div class="basis-1/6 flex justify-center">
-        {{
-          toPersianNumber(currency == "btc" ? model?.btc.buy : model?.usdt.buy)
-        }}
-        تومان
-      </div>
-      <div class="basis-1/6 flex justify-center">
-        %{{ toPersianNumber(model?.fee) }}
-      </div>
-      <div class="basis-1/6 flex justify-center">{{ model?.type }}</div>
-      <div class="basis-1/6 flex justify-center">
-        {{ toPersianNumber(model?.coinsCount) }}
-      </div>
-      <div class="basis-1/6 flex justify-center">
+      <div class="flex flex-row py-2 mx-1 md:hidden">
         <button class="bg-[#4CAF50] text-white p-1 w-1/2">خرید</button>
         <button class="bg-[#DC3545] text-white p-1 mr-1 w-1/2">فروش</button>
       </div>
